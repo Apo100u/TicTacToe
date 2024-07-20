8f76be9bf41719c82a91feb446c02a0848db4ad3
Apo100u <mailodspamu@o2.pl>
2024-07-20 17:46:43 +0200

    add interactions to buttons

--- Assets/Scripts/MVC/Controller/TicTacToeController.cs
+++ Assets/Scripts/MVC/Controller/TicTacToeController.cs
@@ -1,9 +1,38 @@
+using TicTacToe.MVC.View;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace TicTacToe.MVC.Controller
 {
     public class TicTacToeController : MonoBehaviour
     {
-        
+        [SerializeField] private TicTacToeView view;
+
+        [SerializeField] private SymbolWidget[] symbolWidgets;
+        [SerializeField] private GridWidget[] gridButtons;
+        [SerializeField] private int gridSize;
+
+        public void Init()
+        {
+            view.Init(symbolWidgets, gridSize);
+
+            SetUpInteractions(gridButtons);
+        }
+
+        private void SetUpInteractions(GridWidget[] gridButtons)
+        {
+            for (int i = 0; i < gridButtons.Length; i++)
+            {
+                int gridButtonIndex = i;
+                Button button = gridButtons[i].GetComponent<Button>();
+                
+                button.onClick.AddListener(() => OnInteractionButtonClicked(gridButtons[gridButtonIndex]));
+            }
+        }
+
+        private void OnInteractionButtonClicked(GridWidget gridButton)
+        {
+            
+        }
     }
 }

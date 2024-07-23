453e4a912dc4faaad57875cbfca510aafd8ff045
Apo100u <mailodspamu@o2.pl>
2024-07-23 03:27:37 +0200

    update GameSettings SO

--- Assets/Scripts/ScriptableObjects/HelperStructs/Balance.cs
+++ Assets/Scripts/ScriptableObjects/HelperStructs/Balance.cs
@@ -6,7 +6,6 @@ namespace TicTacToe.ScriptableObjects.HelperStructs
     [Serializable]
     public struct Balance
     {
-        [field: Header("Balance")]
         [field: SerializeField] public float TurnTime { get; private set; }
     }
 }
